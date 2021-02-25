2d224e6ebe03cd04904bdf1d830dfc0f5456f317
MarcusGH <29378769+marcusGH@users.noreply.github.com>
2021-02-25 20:46:23 +0100

    fix bug

--- get_output.sh
+++ get_output.sh
@@ -1,4 +1,4 @@
 #use: ./get_output.sh path/to/solver.py
 for letter in a b c d e f ; do
-    python3 "$i" "in/$letter.in"  | tail -n +3 > "out/$letter.out"
+    python3 "$1" "in/$letter.in"  | tail -n +3 > "out/$letter.out"
 done
