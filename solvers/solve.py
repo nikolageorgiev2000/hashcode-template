8b2ecf09dfcbe069d89c9211fbdbe104051f4708
Karl Mose <karl.h.mose@gmail.com>
2021-02-25 18:51:36 +0000

    Added basic solver

--- solvers/solve.py
+++ solvers/solve.py
@@ -13,7 +13,38 @@ def solve(inp, args):
     random.seed(args['seed'])
     ns = parse(inp)
 
-    return '0'
+    schedule = []
+
+    ############## Calculates street usage
+    ############## For future use
+    street_usage = {}
+    for street in ns.streets:
+        street_usage[street] = 0
+
+    for path in ns.cars:
+        for street in path:
+            street_usage[street]+=1
+    ###############
+    ###############
+
+    # Creates round robin scheduling
+    for intersection in range(0,ns.I):
+        node = ns.nodes[intersection]
+        streetTimings = []
+        for street in node.i:
+            streetTimings.append({"name": street, "time": 1})
+
+        schedule.append({"id": intersection, "streets": streetTimings})
+
+    # Creates output string
+    output = str(len(schedule))
+    for intersection in schedule:
+        output += str(intersection["id"]) + "\n"
+        output += str(len(intersection["streets"])) + "\n"
+        for street in intersection["streets"]:
+            output += street["name"] + " " + str(street["time"]) + "\n"
+
+    return output
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
